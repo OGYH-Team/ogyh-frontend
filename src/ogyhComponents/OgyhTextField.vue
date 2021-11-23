00040433dc7f71790487c32963e4de91a4d5a61e
Nice <nicenicegame@gmail.com>
2021-11-23 19:40:08 +0700

    Add auth components

--- src/ogyhComponents/OgyhTextField.vue
+++ src/ogyhComponents/OgyhTextField.vue
@@ -1,12 +1,30 @@
 <template>
-  <input type="text" />
+  <input
+    v-bind="$attrs"
+    v-on="$listeners"
+    class="
+      border border-transparent
+      shadow-md
+      rounded-md
+      w-full
+      px-3
+      py-2
+      focus:outline-none focus:ring-2 focus:ring-purple-600
+    "
+  />
 </template>
 
 <script>
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'OgyhTextField'
+  name: 'OgyhTextField',
+  props: ['value'],
+  methods: {
+    onTextInputChange(e) {
+      this.$emit(e.target.value)
+    }
+  }
 })
 </script>
 
--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,14 +1,30 @@
 <template>
-  <div>
-    <p>Hello</p>
+  <div class="max-w-sm mx-auto flex flex-col items-center justify-center pt-8">
+    <ogyh-text-field
+      type="text"
+      class="my-2"
+      name="username"
+      placeholder="Username"
+    />
+    <ogyh-text-field
+      type="password"
+      class="my-2"
+      name="password"
+      placeholder="Password"
+    />
+    <button class="btn btn-accent my-2">Sign-in</button>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
+import OgyhTextField from '@/ogyhComponents/OgyhTextField.vue'
 
 export default Vue.extend({
   name: 'Account',
+  components: {
+    OgyhTextField
+  },
   props: {
     isSignIn: {
       type: Boolean,
